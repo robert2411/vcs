5f5140ab7000864430ac4d7fbe18dbe1872701d6
Robert Stevens <robert@stevens.tk>
2022-10-31 22:54:41 +0000

    Added some basic features (#7)

--- vcs/src/main/kotlin/com/github/robert241/vcs/VCS.kt
+++ vcs/src/main/kotlin/com/github/robert241/vcs/VCS.kt
@@ -5,8 +5,16 @@ interface VCS {
     fun getHistory(): VCSHistory
     fun getAllBranches(): List<String>
     fun getCurrentBranch(): String
+
+
     fun switchBranch(branch: String)
     fun fetch()
     fun pull()
+
+    fun revertChanges()
+
+    fun addAllFiles()
+    fun commit(commitMessage: String)
+    fun push(pushTags: Boolean)
 
 }
